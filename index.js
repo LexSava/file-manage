8ed6057f5edafa972da71d480821f75c3d5e4a60
Alexei Savostyanov <lexsava@gmail.com>
2024-02-05 14:54:42 +0100

    feat: add functionality index.js
    At the start of the program and after each end of input/operation current working directory should be printed in following way:
    You are currently in path_to_working_directory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const args = process.argv.slice(2);
 const readline = require("readline");
+const path = require("path");
 
 // Function to extract the value of a CLI argument
 const getArgValue = (argName) => {
@@ -10,7 +11,14 @@ const getArgValue = (argName) => {
 // Get the value of the --username argument
 const username = getArgValue("username");
 
-// Display the welcome message
+// Function to print the current working directory
+const printCurrentDirectory = () => {
+  const currentDirectory = process.cwd();
+  console.log("\x1b[33m%s\x1b[0m", `You are currently in ${currentDirectory}`);
+};
+
+// Display the welcome message and current working directory
+printCurrentDirectory();
 console.log(
   "\x1b[32m%s\x1b[0m",
   `Welcome to the File Manager, ${username || "Guest"}!`
