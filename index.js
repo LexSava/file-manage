b703c74d8acbf0bcd9a747c577fcf0c531322905
Alexei Savostyanov <lexsava@gmail.com>
2024-02-05 14:52:22 +0100

    feat: update index.js
    After starting the program displays the following text in the console (Username is equal to value that was passed on application start in --username CLI argument)
    Welcome to the File Manager, Username!
    After program work finished (ctrl + c pressed or user sent .exit command into console) the program displays the following text in the console
    Thank you for using File Manager, Username, goodbye!

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-const usernameArgIndex = process.argv.indexOf("--username");
-const username =
-  usernameArgIndex !== -1 ? process.argv[usernameArgIndex + 1] : "Guest";
+const args = process.argv.slice(2);
+const readline = require("readline");
 
-console.log(`Welcome to the File Manager, ${username}!`);
+// Function to extract the value of a CLI argument
+const getArgValue = (argName) => {
+  const argIndex = args.findIndex((arg) => arg.startsWith(`--${argName}`));
+  return argIndex !== -1 ? args[argIndex].split("=")[1] : null;
+};
+
+// Get the value of the --username argument
+const username = getArgValue("username");
+
+// Display the welcome message
+console.log(
+  "\x1b[32m%s\x1b[0m",
+  `Welcome to the File Manager, ${username || "Guest"}!`
+);
+
+// Create interface for reading from the console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Handle Ctrl+C interrupt
+rl.on("SIGINT", () => {
+  console.log(
+    "\x1b[36m%s\x1b[0m",
+    `Thank you for using File Manager, ${username || "Guest"}, goodbye!`
+  );
+  process.exit();
+});
+
+// Handle program close
+rl.on("close", () => {
+  console.log(
+    "\x1b[36m%s\x1b[0m",
+    `Thank you for using File Manager, ${username || "Guest"}, goodbye!`
+  );
+  process.exit();
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.error(err);
+  process.exit(1);
+});
